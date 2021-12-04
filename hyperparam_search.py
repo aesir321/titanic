7d4383087972e0bcc7570109d629dea5b6d4e03f
Will Parker <parkerwill625@gmail.com>
2021-12-04 16:30:22 +0100

    randome forest hyperparam search

--- hyperparam_search.py
+++ hyperparam_search.py
@@ -4,6 +4,7 @@ from constants import RANDOM_STATE, TARGET
 from sklearn.model_selection import RandomizedSearchCV
 from scipy.stats import randint
 from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 if __name__ == "__main__":
     X_train, y_train = import_data()
@@ -12,7 +13,7 @@ if __name__ == "__main__":
 
     X_train_prepared = pipeline.fit_transform(X_train)
 
-    param_distribs = {
+    param_distribs_gp = {
         "n_restarts_optimizer": randint(low=1, high=200),
         "max_iter_predict": randint(low=10, high=200),
         "warm_start": [True, False],
@@ -20,7 +21,31 @@ if __name__ == "__main__":
 
     rnd_search = RandomizedSearchCV(
         GaussianProcessClassifier(),
-        param_distributions=param_distribs,
+        param_distributions=param_distribs_gp,
+        cv=5,
+        random_state=RANDOM_STATE,
+        scoring="f1",
+        n_jobs=-1,
+    )
+
+    rnd_search.fit(X_train_prepared, y_train)
+    cvres = rnd_search.cv_results_
+
+    for f1, params in zip(cvres["mean_test_score"], cvres["params"]):
+        print(f1, params)
+
+    param_distribs_rf = {
+        "n_estimators": randint(low=1, high=300),
+        "criterion": ["gini", "entropy"],
+        "max_depth": randint(low=1, high=100),
+        "min_samples": randint(low=2, high=100),
+        "max_features": ["auto", "sqrt", "log2"],
+        "bootstrap": [True, False],
+    }
+
+    rnd_search = RandomizedSearchCV(
+        RandomForestClassifier(),
+        param_distributions=param_distribs_gp,
         cv=5,
         random_state=RANDOM_STATE,
         scoring="f1",
