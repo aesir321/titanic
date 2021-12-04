a7e15639895d8c53cbd9a8b1e0927a82df0f9d9b
Will Parker <parkerwill625@gmail.com>
2021-12-04 15:50:19 +0100

    Changed to use pandas instead of numpy

--- AttributeAdder.py
+++ AttributeAdder.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
-cabin_index = 10
+cabin_index = 3
 
 
 class AttributeAdder(BaseEstimator, TransformerMixin):
@@ -10,6 +10,9 @@ class AttributeAdder(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         # https://stackoverflow.com/a/48320451/604365
-        ship_section = X[:, cabin_index].astype("<U1")  # Not sure this works
-
-        return np.c_[X, ship_section]
+        # print(X)
+        # print(type(X))
+        # ship_section = X[:, cabin_index].astype("<U1")  # Not sure this works
+        X["ship_section"] = X["Cabin"].fillna("U").str[0]
+        X.drop(axis=1, columns="Cabin", inplace=True)
+        return X
